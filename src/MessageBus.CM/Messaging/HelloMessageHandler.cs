2f69c0195c4233a030a96b7101f371fcaeab69ce
Serhii Shushliapin <sshushlyapin@gmail.com>
2019-06-24 10:16:48 +0300

    Configure MessageBus

--- src/MessageBus.CM/Messaging/HelloMessageHandler.cs
+++ src/MessageBus.CM/Messaging/HelloMessageHandler.cs
@@ -1,16 +1,25 @@
 ﻿using System.Threading.Tasks;
+using Sitecore.Abstractions;
 using Sitecore.Framework.Messaging;
 
 namespace MessageBus.CM.Messaging
 {
     public class HelloMessageHandler : IMessageHandler<HelloMessage>
     {
+        private readonly BaseLog _logger;
+
+        public HelloMessageHandler(BaseLog logger)
+        {
+            _logger = logger;
+        }
+
         public Task Handle(
             HelloMessage message,
             IMessageReceiveContext receiveContext,
             IMessageReplyContext replyContext)
         {
-            throw new System.NotImplementedException();
+            _logger.Info($"[MessageBusSample]: Message received: '{message.Text}'", this);
+            return Task.CompletedTask;
         }
     }
 }
